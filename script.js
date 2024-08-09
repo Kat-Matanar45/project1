061a3b2fc41f801bf14978949d0a61c0f24a82af
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-09 16:39:55 +0500

    высчитан процент отката

--- script.js
+++ script.js
@@ -7,4 +7,18 @@ let adaptive = true;
 
 alert ("Первое задание в процессе выполнения");
 
-console.log ("Продолжай в том же духе... :-)")
+console.log ("Продолжай в том же духе... :-)");
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/долларов/гривен/юани');
+console.log('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/долларов/гривен/юани');
+
+screens = screens.toLowerCase();
+let screensA = screens.split(', ');
+console.log(screensA);
+
+console.log("Процент отката посреднику за работу" + " " + (fullPrice * (rollback/100)));
