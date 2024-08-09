40b8909ff311f46e050cac2478f1fb7715906bd6
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-09 15:55:05 +0500

    присвоено значение переменным

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 15;
+let rollback = 66;
+let fullPrice = 3000;
+let adaptive = true;
 
 alert ("Первое задание в процессе выполнения");
 
