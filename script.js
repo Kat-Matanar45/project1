8a4b1eb1c15c17a4f35ef58f76cddf38e4a243e2
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-12 22:07:31 +0500

    Все суммы посчитаны без ошибок

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let title = "project";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 15;
-let rollback = 66;
-let fullPrice = 3000;
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = + prompt('Сколько будет стоить данная работа?');
+let rollback = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = + prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = + prompt('Сколько это будет стоить?');
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
 let adaptive = true;
 
 alert ("Первое задание в процессе выполнения");
@@ -22,3 +27,20 @@ let screensA = screens.split(', ');
 console.log(screensA);
 
 console.log("Процент отката посреднику за работу" + " " + (fullPrice * (rollback/100)));
+
+console.log(servicePercentPrice);
+
+switch (true) {
+    case fullPrice >= 30000:
+        console.log("Даем скидку в 10% \nИтоговая сумма за работу:" + " " + (fullPrice - (fullPrice/100*10)));
+        break
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log("Даем скидку в 5% \nИтоговая сумма за работу:" + " " + (fullPrice - (fullPrice/100*5)));
+        break
+    case fullPrice >= 0 && fullPrice < 15000:
+            console.log("Скидка не предусмотрена \nИтоговая сумма за работу:" + " " + fullPrice);
+            break
+    case fullPrice < 0:
+            console.log("Что-то пошло не так");
+            break
+};
