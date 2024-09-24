1d231e43e92d2ee36ab75bd334a1c5d1dd754869
Kat Tyushnyakova <avchimchik@gmail.com>
2024-09-24 19:03:42 +0500

    Правки от Александра

--- js/script.js
+++ js/script.js
@@ -36,34 +36,39 @@ const appData = {
     servicePricesPercent: 0,
     servicePricesNumber: 0,
     rollback: 0,
-    init: function() {
+    init: function () {
         this.addTitle();
 
-        plus.addEventListener('click', this.addSreenBlock); 
-        btnOk.addEventListener('click', this.inError);
-
-        inputRange.addEventListener('input', this.rangeSpan);
+        plus.addEventListener('click', this.addSreenBlock.bind(this));
+        btnOk.addEventListener('click', this.inError.bind(this));
+        inputRange.addEventListener('input', this.rangeSpan.bind(this));
     },
-    rangeSpan: function(event) {
+    rangeSpan: function (event) {
         spanRange.textContent = event.target.value + "%";
-        appData.rollback = event.target.value;
-    },
-    inError: function() {
-       screen = document.querySelectorAll('.screen');
 
+        this.rollback = event.target.value;
+    },
+    inError: function () {
         let error = false;
+
+        screen = document.querySelectorAll('.screen');
 
         screen.forEach((screenen) => {
             const select = screenen.querySelector('select');
             const input = screenen.querySelector('input');
-            
-        if ((select.value === '') || (input.value === '')) {error = true};
+
+            if ((select.value === '') || (input.value === '')) { 
+                error = true 
+            };
         });
 
-        if (error === false) {appData.start()} 
-        else {alert("Заполните все поля!")};
+        if (error === false) { 
+            this.start() 
+        } else { 
+            alert("Заполните все поля!") 
+        };
     },
-    addScreens: function() {
+    addScreens: function () {
         screen = document.querySelectorAll('.screen');
 
         screen.forEach((screenen, index) => {
@@ -71,28 +76,31 @@ const appData = {
             const input = screenen.querySelector('input');
             const selectName = select.options[select.selectedIndex].textContent;
 
-        this.screens.push({ 
-            id: index, 
-            name: selectName, 
-            value: +select.value * +input.value,
-            count: +input.value});
+            this.screens.push({
+                id: index,
+                name: selectName,
+                value: +select.value * +input.value,
+                count: +input.value
+            });
         });
     },
-    sumCount: function(sum) {
+    sumCount: function (sum) {
         sum = 0;
-        for (let i=0; i<this.screens.length; i++) {
-           sum = this.screens[i].count + sum;
+
+        for (let i = 0; i < this.screens.length; i++) {
+            sum = this.screens[i].count + sum;
         };
+
         return sum;
     },
-    addServices: function() {
+    addServices: function () {
         percent.forEach((item) => {
             const check = item.querySelector('input[type=checkbox]');
             const label = item.querySelector('label');
             const input = item.querySelector('input[type=text]');
 
             if (check.checked) {
-            this.servicesPercent[label.textContent] = +input.value;
+                this.servicesPercent[label.textContent] = +input.value;
             };
         });
 
@@ -102,23 +110,22 @@ const appData = {
             const input = item.querySelector('input[type=text]');
 
             if (check.checked) {
-            this.servicesNumber[label.textContent] = +input.value;
+                this.servicesNumber[label.textContent] = +input.value;
             };
         })
-
-        console.log(appData)
     },
-    addSreenBlock: function() {
+    addSreenBlock: function () {
         const cloneScreen = screen[0].cloneNode(true);
+        
         screen[screen.length - 1].after(cloneScreen);
     },
-    addTitle: function() {
+    addTitle: function () {
         document.title = title.textContent;
     },
-    addPrices: function() {
+    addPrices: function () {
         this.screenPrice = this.screens.reduce(function (sum, screens) {
             return sum + screens.value
-          }, 0);
+        }, 0);
 
         for (let key in this.servicesNumber) {
             this.servicePricesNumber += this.servicesNumber[key]
@@ -128,39 +135,29 @@ const appData = {
             this.servicePricesPercent += this.screenPrice * (this.servicesPercent[key] / 100);
         }
 
-        appData.fullPrice = appData.screenPrice + appData.servicePricesNumber + appData.servicePricesPercent;
+        this.fullPrice = this.screenPrice + this.servicePricesNumber + this.servicePricesPercent;
 
-        appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
+        this.servicePercentPrice = this.fullPrice - (this.fullPrice * (this.rollback / 100));
     },
-    showResult: function() {
+    showResult: function () {
         inputTotal.value = this.screenPrice;
         inputOther.value = this.servicePricesNumber + this.servicePricesPercent;
         inputFullCount.value = this.fullPrice;
         inputRollback.value = this.servicePercentPrice;
         inputCount.value = this.sumCount();
     },
-    logger: function() {
-            console.log(appData.fullPrice);
-            console.log(appData.servicePercentPrice);
-            console.log(appData.screens);
-            console.log(appData.services);
-            },
-    start: function() {
-       this.addScreens();
-       this.addServices(); 
-       this.addPrices();
-       this.showResult();
-
-       // appData.logger();
+    logger: function () {
+        console.log(this.fullPrice);
+        console.log(this.servicePercentPrice);
+        console.log(this.screens);
+        console.log(this.services);
+    },
+    start: function () {
+        this.addScreens();
+        this.addServices();
+        this.addPrices();
+        this.showResult();
     }
 }
 
-
 appData.init();
-
-// btn.addEventListener('click', appData.init.bind(appData))
-// const newInit = appData.bind(init);
-
-// newInit();
-
-
